1c916bd1390751be34d3eca72dc4a713bb3a706d
kvnal <72142540+kvnal@users.noreply.github.com>
2021-05-21 15:49:36 +0530

    file sizes & thread loading added

--- main.py
+++ main.py
@@ -7,8 +7,23 @@ import argparse
 import getpass
 import os
 import base64
+import threading
+import itertools
+import time
+import sys
 
-parser = argparse.ArgumentParser()
+global done
+done=False
+def animate():
+    msg="Processing "
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            sys.stdout.flush()
+            sys.stdout.write(f'\rDone!                      ')
+            break
+        sys.stdout.write(f'\r {msg}' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
 
 def getQA():
     return [str(input("security Question : ")),str(input("Answer : "))]
@@ -19,6 +34,9 @@ def getDevicePass(path):
     key = base64.b64decode(key).decode()
     return key
 
+
+    
+parser = argparse.ArgumentParser()
 if __name__ == "__main__":
     parser.add_argument("path",type=str)    
     parser.add_argument("-e","--encrypt",action="store_true")
@@ -28,15 +46,26 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
     print(args.device)
+    t= threading.Thread(target= animate)
     if (ntpath.isfile(args.path)):
         # encrypt
+
         if(args.encrypt):
             print("use '-reverseTime-' if you want password to be in mm:hh format (24 hr fomat)")
             password = getpass.getpass("Password : ")
             if (password == getpass.getpass("confirm Password : ")):
                 q,a = getQA()
-                edObj = ed.main(args.path,password).encrypt(q,a)
-
+                
+                fileSize = os.path.getsize(args.path)
+                estimatedFileSize = fileSize*33.3//100 + fileSize
+                print(f"File Size {fileSize} (bytes) estimated encrypted file size {estimatedFileSize} (bytes)")
+                t2 = threading.Thread(target=ed.main(args.path,password).encrypt(q,a))
+                t.start()
+                t2.start()
+                t2.join()
+                done=True
+                # edObj = ed.main(args.path,password).encrypt(q,a)
+                # animate.done=True
                 if(args.device):
                     os.chdir(args.device)
                     os.mkdir(".fileSec")
@@ -54,7 +83,12 @@ if __name__ == "__main__":
             if(not args.forgot):
                 
                 password = getpass.getpass("Password : ") if not args.device else getDevicePass(args.device)
-                edObj = ed.main(args.path,password).decrypt()
+                t2 = threading.Thread(target=ed.main(args.path,password).decrypt())
+                t.start()
+                t2.start()
+                t2.join()
+                done=True
+                # edObj = ed.main(args.path,password).decrypt()
             else:
                 edObj = ed.main(args.path,"").forgotPass()
                 print(edObj)
